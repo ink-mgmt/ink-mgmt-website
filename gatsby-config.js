4ca012cf6dd35bd23b094f3e8787354807f1dfbf
Mike Riley <mikeriley131@gmail.com>
2022-04-21 20:23:14 -0600

    adds favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,7 @@ module.exports = {
         background_color: `#0000f5`,
         theme_color: `#000000`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/ink-mgmt-favicon.svg`, // This path is relative to the root of the site.
       },
     },
     {
