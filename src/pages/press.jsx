c6e8dd3c7871bcbae718d0543c69d18c219e5f86
Mike Riley <mikeriley131@gmail.com>
2022-04-22 20:42:48 -0600

    vertically centers press content in article

--- src/pages/press.jsx
+++ src/pages/press.jsx
@@ -14,160 +14,166 @@ const Press = () => (
     <div className="press__wrapper">
       <h1 className="press__heading visuallyhidden">Word of Mouth</h1>
       <article className="press__article">
-        <h2 className="press__publication">The New Yorker</h2>
-        <p className="press__date">11-14-21</p>
-        <div className="press__content-wrapper">
-          <p className="press__title">
-            Why the Rap Veterans Behind Atlanta Indie Label Quality Control
-            Music Are the Smartest Guys in Hip-Hop
-          </p>
-          <div className="press__copy">
-            <p>
-              ATLANTA — Unless you catch a glimpse of the eggplant
-              Mercedes-Maybach S600 or the various young men with clusters of
-              diamonds on choker-short chains coming and going at all hours,
-              there is nothing too flashy about the headquarters of Quality
-              Control Music, a record label here in the world’s de facto hip-hop
-              capital.
+        <div className="press__article-content">
+          <h2 className="press__publication">The New Yorker</h2>
+          <p className="press__date">11-14-21</p>
+          <div className="press__content-wrapper">
+            <p className="press__title">
+              Why the Rap Veterans Behind Atlanta Indie Label Quality Control
+              Music Are the Smartest Guys in Hip-Hop
             </p>
+            <div className="press__copy">
+              <p>
+                ATLANTA — Unless you catch a glimpse of the eggplant
+                Mercedes-Maybach S600 or the various young men with clusters of
+                diamonds on choker-short chains coming and going at all hours,
+                there is nothing too flashy about the headquarters of Quality
+                Control Music, a record label here in the world’s de facto
+                hip-hop capital.
+              </p>
 
-            <p>
-              As the birthplace of the chart-topping, trendsetting careers of
-              Migos and Lil Yachty, this studio and office compound, northwest
-              of downtown, is the latest nondescript landmark to help alter the
-              course of rap music, a near-constant occurrence in Atlanta over
-              the last two decades. But despite its pedigree as a center of
-              luxury and innovation, the space — tucked behind a Goodwill and a
-              full-service dog care facility — is light on bacchanalia and heavy
-              on rules and expectations.
-            </p>
+              <p>
+                As the birthplace of the chart-topping, trendsetting careers of
+                Migos and Lil Yachty, this studio and office compound, northwest
+                of downtown, is the latest nondescript landmark to help alter
+                the course of rap music, a near-constant occurrence in Atlanta
+                over the last two decades. But despite its pedigree as a center
+                of luxury and innovation, the space — tucked behind a Goodwill
+                and a full-service dog care facility — is light on bacchanalia
+                and heavy on rules and expectations.
+              </p>
 
-            <p>
-              “DO NOT come to the studio UNLESS you are working,” reads a
-              weathered printout taped to a bare wall amid four recording
-              studios. “BE RESPONSIBLE for the company you bring … DO NOT have
-              anyone dropping off or picking up drugs at the studio … This is
-              not your home, this is not a hangout, this is a place of business.
-              PLEASE conduct yourself accordingly and in a professional manner.”
-              (Also: “ANY gambling, all parties involved must pay the house
-              30%!”)
-            </p>
-          </div>
+              <p>
+                “DO NOT come to the studio UNLESS you are working,” reads a
+                weathered printout taped to a bare wall amid four recording
+                studios. “BE RESPONSIBLE for the company you bring … DO NOT have
+                anyone dropping off or picking up drugs at the studio … This is
+                not your home, this is not a hangout, this is a place of
+                business. PLEASE conduct yourself accordingly and in a
+                professional manner.” (Also: “ANY gambling, all parties involved
+                must pay the house 30%!”)
+              </p>
+            </div>
 
-          <div className="press__link-wrapper">
-            <a
-              className="press__link link"
-              href="/"
-              aria-label="Read more from TITLE"
-            >
-              Read more
-            </a>
-          </div>
-        </div>
-      </article>
-
-      <article className="press__article">
-        <h2 className="press__publication">The New Yorker</h2>
-        <p className="press__date">11-14-21</p>
-        <div className="press__content-wrapper">
-          <p className="press__title">
-            Why the Rap Veterans Behind Atlanta Indie Label Quality Control
-            Music Are the Smartest Guys in Hip-Hop
-          </p>
-          <div className="press__copy">
-            <p>
-              ATLANTA — Unless you catch a glimpse of the eggplant
-              Mercedes-Maybach S600 or the various young men with clusters of
-              diamonds on choker-short chains coming and going at all hours,
-              there is nothing too flashy about the headquarters of Quality
-              Control Music, a record label here in the world’s de facto hip-hop
-              capital.
-            </p>
-
-            <p>
-              As the birthplace of the chart-topping, trendsetting careers of
-              Migos and Lil Yachty, this studio and office compound, northwest
-              of downtown, is the latest nondescript landmark to help alter the
-              course of rap music, a near-constant occurrence in Atlanta over
-              the last two decades. But despite its pedigree as a center of
-              luxury and innovation, the space — tucked behind a Goodwill and a
-              full-service dog care facility — is light on bacchanalia and heavy
-              on rules and expectations.
-            </p>
-
-            <p>
-              “DO NOT come to the studio UNLESS you are working,” reads a
-              weathered printout taped to a bare wall amid four recording
-              studios. “BE RESPONSIBLE for the company you bring … DO NOT have
-              anyone dropping off or picking up drugs at the studio … This is
-              not your home, this is not a hangout, this is a place of business.
-              PLEASE conduct yourself accordingly and in a professional manner.”
-              (Also: “ANY gambling, all parties involved must pay the house
-              30%!”)
-            </p>
-          </div>
-
-          <div className="press__link-wrapper">
-            <a
-              className="press__link link"
-              href="/"
-              aria-label="Read more from TITLE"
-            >
-              Read more
-            </a>
+            <div className="press__link-wrapper">
+              <a
+                className="press__link link"
+                href="/"
+                aria-label="Read more from TITLE"
+              >
+                Read more
+              </a>
+            </div>
           </div>
         </div>
       </article>
 
       <article className="press__article">
-        <h2 className="press__publication">The New Yorker</h2>
-        <p className="press__date">11-14-21</p>
-        <div className="press__content-wrapper">
-          <p className="press__title">
-            Why the Rap Veterans Behind Atlanta Indie Label Quality Control
-            Music Are the Smartest Guys in Hip-Hop
-          </p>
-          <div className="press__copy">
-            <p>
-              ATLANTA — Unless you catch a glimpse of the eggplant
-              Mercedes-Maybach S600 or the various young men with clusters of
-              diamonds on choker-short chains coming and going at all hours,
-              there is nothing too flashy about the headquarters of Quality
-              Control Music, a record label here in the world’s de facto hip-hop
-              capital.
+        <div className="press__article-content">
+          <h2 className="press__publication">The New Yorker</h2>
+          <p className="press__date">11-14-21</p>
+          <div className="press__content-wrapper">
+            <p className="press__title">
+              Why the Rap Veterans Behind Atlanta Indie Label Quality Control
+              Music Are the Smartest Guys in Hip-Hop
             </p>
+            <div className="press__copy">
+              <p>
+                ATLANTA — Unless you catch a glimpse of the eggplant
+                Mercedes-Maybach S600 or the various young men with clusters of
+                diamonds on choker-short chains coming and going at all hours,
+                there is nothing too flashy about the headquarters of Quality
+                Control Music, a record label here in the world’s de facto
+                hip-hop capital.
+              </p>
 
-            <p>
-              As the birthplace of the chart-topping, trendsetting careers of
-              Migos and Lil Yachty, this studio and office compound, northwest
-              of downtown, is the latest nondescript landmark to help alter the
-              course of rap music, a near-constant occurrence in Atlanta over
-              the last two decades. But despite its pedigree as a center of
-              luxury and innovation, the space — tucked behind a Goodwill and a
-              full-service dog care facility — is light on bacchanalia and heavy
-              on rules and expectations.
-            </p>
+              <p>
+                As the birthplace of the chart-topping, trendsetting careers of
+                Migos and Lil Yachty, this studio and office compound, northwest
+                of downtown, is the latest nondescript landmark to help alter
+                the course of rap music, a near-constant occurrence in Atlanta
+                over the last two decades. But despite its pedigree as a center
+                of luxury and innovation, the space — tucked behind a Goodwill
+                and a full-service dog care facility — is light on bacchanalia
+                and heavy on rules and expectations.
+              </p>
 
-            <p>
-              “DO NOT come to the studio UNLESS you are working,” reads a
-              weathered printout taped to a bare wall amid four recording
-              studios. “BE RESPONSIBLE for the company you bring … DO NOT have
-              anyone dropping off or picking up drugs at the studio … This is
-              not your home, this is not a hangout, this is a place of business.
-              PLEASE conduct yourself accordingly and in a professional manner.”
-              (Also: “ANY gambling, all parties involved must pay the house
-              30%!”)
-            </p>
+              <p>
+                “DO NOT come to the studio UNLESS you are working,” reads a
+                weathered printout taped to a bare wall amid four recording
+                studios. “BE RESPONSIBLE for the company you bring … DO NOT have
+                anyone dropping off or picking up drugs at the studio … This is
+                not your home, this is not a hangout, this is a place of
+                business. PLEASE conduct yourself accordingly and in a
+                professional manner.” (Also: “ANY gambling, all parties involved
+                must pay the house 30%!”)
+              </p>
+            </div>
+
+            <div className="press__link-wrapper">
+              <a
+                className="press__link link"
+                href="/"
+                aria-label="Read more from TITLE"
+              >
+                Read more
+              </a>
+            </div>
           </div>
+        </div>
+      </article>
 
-          <div className="press__link-wrapper">
-            <a
-              className="press__link link"
-              href="/"
-              aria-label="Read more from TITLE"
-            >
-              Read more
-            </a>
+      <article className="press__article">
+        <div className="press__article-content">
+          <h2 className="press__publication">The New Yorker</h2>
+          <p className="press__date">11-14-21</p>
+          <div className="press__content-wrapper">
+            <p className="press__title">
+              Why the Rap Veterans Behind Atlanta Indie Label Quality Control
+              Music Are the Smartest Guys in Hip-Hop
+            </p>
+            <div className="press__copy">
+              <p>
+                ATLANTA — Unless you catch a glimpse of the eggplant
+                Mercedes-Maybach S600 or the various young men with clusters of
+                diamonds on choker-short chains coming and going at all hours,
+                there is nothing too flashy about the headquarters of Quality
+                Control Music, a record label here in the world’s de facto
+                hip-hop capital.
+              </p>
+
+              <p>
+                As the birthplace of the chart-topping, trendsetting careers of
+                Migos and Lil Yachty, this studio and office compound, northwest
+                of downtown, is the latest nondescript landmark to help alter
+                the course of rap music, a near-constant occurrence in Atlanta
+                over the last two decades. But despite its pedigree as a center
+                of luxury and innovation, the space — tucked behind a Goodwill
+                and a full-service dog care facility — is light on bacchanalia
+                and heavy on rules and expectations.
+              </p>
+
+              <p>
+                “DO NOT come to the studio UNLESS you are working,” reads a
+                weathered printout taped to a bare wall amid four recording
+                studios. “BE RESPONSIBLE for the company you bring … DO NOT have
+                anyone dropping off or picking up drugs at the studio … This is
+                not your home, this is not a hangout, this is a place of
+                business. PLEASE conduct yourself accordingly and in a
+                professional manner.” (Also: “ANY gambling, all parties involved
+                must pay the house 30%!”)
+              </p>
+            </div>
+
+            <div className="press__link-wrapper">
+              <a
+                className="press__link link"
+                href="/"
+                aria-label="Read more from TITLE"
+              >
+                Read more
+              </a>
+            </div>
           </div>
         </div>
       </article>
