49b49e29bf73b4b28a3380dbbabb57be8e540a6f
Mike Riley <mikeriley131@gmail.com>
2022-07-26 20:07:09 -0600

    attempts to fix homepage background on mobile

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,6 +10,8 @@ import IntroText from '../components/home/introText';
 import WhatWeDo from '../components/home/whatWeDo';
 import GetInTouch from '../components/home/getInTouch';
 
+import mobileBgImg from '../images/blue-paper-bg-vert.jpg';
+
 const IndexPage = ({ location }) => {
   const [heroIsScrolled, setHeroIsScrolled] = useState(false);
 
@@ -22,6 +24,7 @@ const IndexPage = ({ location }) => {
       heroIsScrolled={heroIsScrolled}
     >
       <SEO title="Home" meta={[{ name: 'theme-color', content: '#1601f8' }]} />
+      <img src={mobileBgImg} alt="" className="home__mobile-bg" />
       <InView rootMargin="0px 0px -100% 0px">
         {({ inView, ref }) => (
           <Hero
