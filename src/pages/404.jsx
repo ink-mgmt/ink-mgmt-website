b62472deaf197931d05546f8665fa1a0670f19f5
Mike Riley <mikeriley131@gmail.com>
2023-01-14 12:11:17 -0700

    updates 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,9 +1,16 @@
-const NotFoundPage = () => {
-  if (typeof window !== 'undefined') {
-    window.location = '/';
-  }
+import React, { useEffect } from 'react';
+import { navigate } from 'gatsby';
 
-  return null;
+const NotFoundPage = () => {
+  useEffect(() => {
+    navigate('/'); // redirecting to home page
+  }, []);
+
+  return (
+    <div>
+      <h1>(404) NotFound Page</h1>
+    </div>
+  );
 };
 
 export default NotFoundPage;
